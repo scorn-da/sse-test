5a5469c48a29f847ec45b4edd3b83c76f5016495
Dmitriy <varganovda@gmail.com>
2024-07-24 00:49:09 +0300

    feat(code): added a server side to send messages

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+function sse(req, res) {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  
+  let id = 0;
+  
+  res.write('data: Dmitriy\'s data\n');
+  res.write(`last succeed message id: ${++id} \n`);
+  res.write('\n');
+}
+
 http.createServer((req, res) => {
+  const url = new URL(`http://${req.headers.host}${req.url}`);
+  
+  if (url.pathname === '/stream') {
+    sse(req, res);
+    return;
+  }
+  
   const fileStream = fs.createReadStream(path.join(__dirname, 'index.html'));
   fileStream.pipe(res);
 }).listen(8080, () => {
